6d9558e64f0c538d50103e4ee3ae3ba564d26e30
蒲德军 <xiaozhi2015@hotmail.com>
2024-07-25 17:46:01 +0800

    update

--- resources/views/tree/branch.blade.php
+++ resources/views/tree/branch.blade.php
@@ -1,15 +1,13 @@
-@if($branch['group'] == $group)
-    <li class="dd-item" data-id="{{ $branch[$keyName] }}">
-        <div class="dd-handle">
-            {!! $branchCallback($branch) !!}
-            {!! $actions->setRow($branch)->display($actionsCallback) !!}
-        </div>
-        @if(isset($branch['children']))
-            <ol class="dd-list">
-                @foreach($branch['children'] as $branch)
-                    @include($branchView,  $branch)
-                @endforeach
-            </ol>
-        @endif
-    </li>
-@endif
+<li class="dd-item" data-id="{{ $branch[$keyName] }}">
+    <div class="dd-handle">
+        {!! $branchCallback($branch) !!}
+        {!! $actions->setRow($branch)->display($actionsCallback) !!}
+    </div>
+    @if(isset($branch['children']))
+        <ol class="dd-list">
+            @foreach($branch['children'] as $branch)
+                @include($branchView,  $branch)
+            @endforeach
+        </ol>
+    @endif
+</li>
